ae13970df909ddfde3318f3948f81b6b8584304e
roma-agrawal <agrawalroma314@gmail.com>
2026-07-22 19:42:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,63 @@
-self.addEventListener("install",e=>{
+const CACHE_NAME = "roam-with-roma-v1";
 
-console.log("Roam with Roma Installed");
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./app.js",
+
+    "./tripData.js",
+
+    "./manifest.json"
+
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+
+    );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+
+            Promise.all(
+
+                keys
+
+                .filter(key => key !== CACHE_NAME)
+
+                .map(key => caches.delete(key))
+
+            )
+
+        )
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response => response || fetch(event.request))
+
+    );
 
 });
