a10b1a0ffa0bbff10e801c39056d036ca4753676
roma-agrawal <agrawalroma314@gmail.com>
2026-07-22 17:10:18 +0530

    Create tripData.js

---
+++ tripData.js
@@ -0,0 +1,13 @@
+const trip = {
+  destination: "Switzerland",
+  startDate: "2026-08-17",
+  endDate: "2026-08-25",
+  travelers: [
+    "Roma",
+    "Husband",
+    "Ryan"
+  ],
+  days: [
+    // Day 1, Day 2...
+  ]
+};
